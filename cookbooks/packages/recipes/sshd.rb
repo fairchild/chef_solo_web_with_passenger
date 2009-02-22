ff07610c76e9edf32d07cfb40df0be9836f9d756
John Dewey <john@dewey.ws>
2009-02-22 00:44:24 -0800

    fixed sshd sed

--- cookbooks/packages/recipes/sshd.rb
+++ cookbooks/packages/recipes/sshd.rb
@@ -4,10 +4,11 @@ script "sshd_disable_root_login" do
   cwd "/tmp"
   code <<-EOH
   sed 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config > sshd_config
+  cp sshd_config /etc/ssh/sshd_config
+  chmod 0644 /etc/ssh/sshd_config
+  rm sshd_config
   EOH
 end
-
-# chmod 0644 /etc/ssh/sshd_config
 
 # service "ssh" do
 #   supports :restart => true, :reload => true
