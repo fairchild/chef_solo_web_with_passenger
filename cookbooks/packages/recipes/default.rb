5c0d4d18261f1c5d9a6306ed8fc6ef514edbefde
John Dewey <john@dewey.ws>
2009-02-21 23:16:14 -0800

    added iptables and host package

--- cookbooks/packages/recipes/default.rb
+++ cookbooks/packages/recipes/default.rb
@@ -1,5 +1,5 @@
 ##
 # Additional packages to install.
-%w(iptables irssi zsh strace logrotate sysstat).each do |p|
+%w(iptables irssi zsh strace logrotate sysstat host).each do |p|
   package p
 end
---
+++ cookbooks/packages/recipes/iptables.rb
@@ -0,0 +1,23 @@
+script "iptables_setup" do
+  interpreter "bash"
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+  sudo iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+  sudo iptables -A INPUT -p tcp --dport ssh -j ACCEPT
+  sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+  sudo iptables -A INPUT -j DROP
+  sudo iptables -I INPUT 1 -i lo -j ACCEPT
+  sudo iptables -I INPUT 5 -m limit --limit 5/min -j LOG --log-prefix "iptables denied: " --log-level 7
+  EOH
+end
+
+template "/etc/network/if-post-down.d/iptasave" do
+  mode 0700
+  source "iptasave.erb"
+end
+
+template "/etc/network/if-pre-up.d/iptaload" do
+  mode 0700
+  source "iptaload.erb"
+end
