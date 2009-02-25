443b959abafb355079686869140db594e5e2e9fb
John Dewey <john@dewey.ws>
2009-02-24 23:51:58 -0800

    installing passenger with libapache2-mod-passenger vs script installer

--- cookbooks/packages/recipes/apache2.rb
+++ cookbooks/packages/recipes/apache2.rb
@@ -1,12 +1,12 @@
-%w(apache2 apache2-threaded-dev).each do |p|
+%w(apache2 apache2-threaded-dev libapache2-mod-passenger).each do |p|
   package p
 end
 
 ##
 # Postinstall script.
-bash "install_apache2_module" do
-  code "echo -en '\n\n\n\n\n' | sudo passenger-install-apache2-module"
-end
+# bash "install_apache2_module" do
+#   code "echo -en '\n\n\n\n\n' | sudo passenger-install-apache2-module"
+# end
 
 template "/etc/apache2/conf.d/passenger.conf" do
   mode 0644
