e2cf173a87bef4b5ffee3f22c062aed8c5256cbc
John Dewey <john@dewey.ws>
2009-03-14 15:45:04 -0700

    removed rubygems update - will manage thru apt-get for now

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,6 @@ apt-get update && apt-get upgrade
 apt-get install ruby ruby1.8-dev rubygems libopenssl-ruby1.8 libsqlite3-ruby irb build-essential
 
 ### bootstrap with chef.
-gem install rubygems-update && /var/lib/gems/1.8/bin/update_rubygems
 gem install chef ohai --source http://gems.opscode.com --source http://gems.rubyforge.org
 
 ### Run chef solo.
