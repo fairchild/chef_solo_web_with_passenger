a103710b38038005e77b4c465161dbee7d8cd2ef
John Dewey <john@dewey.ws>
2009-03-22 19:05:59 -0700

    put back rubygems upgrade from e2cf173a87bef4b5ffee3f22c062aed8c5256cbc

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ rm -rf /root/.gem && mkdir /root/.gem && chown 000 /root/.gem
 
 ### bootstrap with ruby.
 apt-get update && apt-get upgrade
+gem install rubygems-update && /var/lib/gems/1.8/bin/update_rubygems
 apt-get install ruby ruby1.8-dev rubygems libopenssl-ruby1.8 libsqlite3-ruby irb build-essential
 
 ### bootstrap with chef.
